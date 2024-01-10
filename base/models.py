7b5ec801d315052cd3ff792d20dca10a4e1524ad
brandon23567 <brandonmahuvava0@gmail.com>
2024-01-10 22:06:16 +0530

    i have now added cloudinary and my postgres database so it should be just fine

--- base/models.py
+++ base/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 class BlogPost(models.Model):
     username = models.CharField(max_length=300)
     title = models.CharField(max_length=300)
     slug = models.CharField(max_length=300)
     short_description = models.TextField()
-    header_image = models.ImageField(upload_to="header_images/", blank=True, null=True)
+    header_image = CloudinaryField("Images", resource_type="image", null=True, blank=True)
     body = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
 
--- website/settings.py
+++ website/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     "corsheaders",
     "rest_framework_simplejwt.token_blacklist",
     'rest_framework_simplejwt',
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -83,13 +85,23 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'StrongestAvenger#23',
+        'HOST': 'db.kxkwjqnbtbcqajzvmkla.supabase.co',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -155,6 +167,15 @@ CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOWED_ORIGINS = [
   "http://localhost:3001",  
 ]
+
+cloudinary.config(
+    cloud_name = 'ddcvsdcdm',
+    api_key = '985629638667792',
+    api_secret = '2qK4waFr0Os4X78xz5t0M9sywAc'
+)
+
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
 SIMPLE_JWT = {
